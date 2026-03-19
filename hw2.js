3f97f440377f2e72fc3def78ce26f99cd0d6b47c
jurgenmondragon-ctrl <jurgenmondragon@gmail.com>
2026-03-19 15:06:24 -0500

    hw2.js

--- hw2.js
+++ hw2.js
@@ -1,1 +1,18 @@
+/*
+   Name:Jurgen Mondragon
+   Date created:
+   Date modified:
+   Purpose: Homework 1 JS
+*/
 
+//dynamic date js code
+const d= new Date();
+let text=d.toLocaleDateString();
+document.getElementById("today").innerHTML = text;
+
+//range slider js code
+let slider = document.getElementById("range");
+    let output = document.getElementById("range-slider");
+    output.innerHTML = slider.value;
+
+    slider.oninput = function () {output.innerHTML = this.value;};
